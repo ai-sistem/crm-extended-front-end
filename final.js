e61bfa191fd50747bdfe77d2b764386eeb15aeec
broseph_ubuntu <joegmulcahy@gmail.com>
2024-09-25 11:41:50 -0400

    Wait for layout element

--- final.js
+++ final.js
@@ -1,7 +1,31 @@
 (function() {
 
-    function onURLChange() {
+    async function waitForElm(selector) {
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+    
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+    
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+
+    async function onURLChange() {
         console.log("URL changed to:", location.href);
+        // Wait for GHL layout element to load
+        const layout = await waitForElm("w-full");
+        console.log("GHL layout element loaded:", layout);
         // Add your URL change handling logic here
     }
     
@@ -11,10 +35,10 @@
     onURLChange();
     
     // Check for URL changes
-    function checkURLChange() {
+    async function checkURLChange() {
         if (lastUrl !== location.href) {
             lastUrl = location.href;
-            onURLChange();
+            await onURLChange();
         }
     }
     
